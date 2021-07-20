a87a8d62a30d28097ec035fb544b6a90e44235c6
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-07-20 18:40:41 +0430

    mining blocks

--- blockchain.py
+++ blockchain.py
@@ -37,8 +37,10 @@ class Block():
                         self.nonce)
                     )
 
+
 class Blockchain():
     difficulty = 4
+
 
     def __init__(self, chain = []):
         self.chain = chain
@@ -51,10 +53,33 @@ class Blockchain():
             'data': block.data, 
             'nonce': block.nonce
             })
-            
+
+    def mine(self, block):
+        try:
+            block.prev_hash = self.chain[-1].get('hash')
+        except IndexError:
+            pass
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block); break
+            else:
+                block.nonce += 1
+
+
 def main():
-    block = Block("trans1", 1)
-    print(block)
+    #block = Block("trans1", 1)
+    #print(block)
+    blockchain = Blockchain()
+    DB = ["hello world", "what's up", "hello", "bye"]
+    num = 0
+    for data in DB:
+        num +=1
+        blockchain.mine(Block(data, num))
+    #print(blockchain.chain)
+    for block in blockchain.chain:
+        print(block)
+
+
 
 if __name__ == "__main__":
     main()
