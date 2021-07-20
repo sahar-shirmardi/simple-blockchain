fb532b42d6c26612670a894c7016a826846a89df
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-07-20 19:30:54 +0430

    10K record database

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from blockchain import Block
 from blockchain import Blockchain
 
 # read data records
-with open('./database/4records.csv', newline='') as f:
+with open('./database/records.csv', newline='') as f:
     reader = csv.reader(f)
     database = list(reader)
 
@@ -21,7 +21,7 @@ def main():
 
     # change data of a block manually
     # validate functioin will report False
-    blockchain.chain[2].data = "NEW DATA"
+    blockchain.chain[776].data = "NEW DATA"
     print(blockchain.validate())
 
     # mine the blockhchain after changing the data
